c0c0352d5a99bea04b052e6fba3df5ed1fa276c3
majkel3321 <majkel3321@gmail.com>
2020-02-18 23:53:24 +0100

    mappers added

--- src/main/java/com/forum/testForum/mapper/PostMapper.java
+++ src/main/java/com/forum/testForum/mapper/PostMapper.java
@@ -1,4 +1,18 @@
 package com.forum.testForum.mapper;
 
+import com.forum.testForum.domain.Post;
+import com.forum.testForum.domain.dto.PostDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class PostMapper {
+
+    public List<PostDto> mapToPostDtoList(List<Post> posts){
+        return posts.stream()
+                .map(post -> new PostDto(post.getId(),post.getSubject(),post.getMessage(),
+                        post.getUser().getId(),post.getDateTime())).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/forum/testForum/mapper/UserMapper.java
+++ src/main/java/com/forum/testForum/mapper/UserMapper.java
@@ -1,4 +1,27 @@
 package com.forum.testForum.mapper;
 
+import com.forum.testForum.domain.User;
+import com.forum.testForum.domain.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
 public class UserMapper {
+
+    @Autowired
+    private PostMapper postMapper;
+
+    public UserDto mapToUserDto(User user){
+        return new UserDto(user.getId(),user.getUsername(),user.getPassword(),user.isAdmin(),
+                user.isModerator(),postMapper.mapToPostDtoList(user.getPosts()),user.getRegistrationDate(),user.getLastLoginDate());
+    }
+
+    public List<UserDto> mapToUserDtoList(List<User> users){
+        return users.stream()
+                .map(user -> new UserDto(user.getId(),user.getUsername(),user.getPassword(),user.isAdmin(),
+                        user.isModerator(),postMapper.mapToPostDtoList(user.getPosts()),user.getRegistrationDate(),user.getLastLoginDate())).collect(Collectors.toList());
+    }
 }
