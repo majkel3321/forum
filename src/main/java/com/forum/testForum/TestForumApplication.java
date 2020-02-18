cdb6b5811f2e85d8c834e6cf475f5fb3430c9029
majkel3321 <majkel3321@gmail.com>
2020-02-18 19:18:13 +0100

    first commit

---
+++ src/main/java/com/forum/testForum/TestForumApplication.java
@@ -0,0 +1,13 @@
+package com.forum.testForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forum/testForum/TestForumApplicationTests.java
@@ -0,0 +1,16 @@
+package com.forum.testForum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
